697662bab4cacc35b075ec001ee014e5e0578d2a
Oscar Alonso <31137965+oiab94@users.noreply.github.com>
2024-08-25 11:42:56 -0400

    Update keymaps.lua

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -35,19 +35,12 @@ map('n', '<C-c>', '<CMD>Neotree close<CR>')
 
 
 -- LSP:
-<<<<<<< HEAD
 map('n', 'K', vim.lsp.buf.hover)						-- Muestra información acerca del simbolo debajo del cursor en una pantalla flotante
 map('n', 'gd', vim.lsp.buf.definition)			-- Va a la definición del simbolo que se encuetra debajo del cursor
 map('n', 'gD', vim.lsp.buf.declaration) 		-- Va la declaración del simbolo que se encuentra debajo del cursor
 map('n', '<leader>rn', vim.lsp.buf.rename) 	-- Renombra todas las referencias que se encuentra debajo del cursor
 map('n', 'gr', vim.lsp.buf.references) 			-- Lista todas las referencias que se encuentra debajo del cursor
-=======
-map('n', 'K', vim.lsp.buf.hover, { buffer=0 })				-- Muestra información acerca del simbolo debajo del cursor en una pantalla flotante
-map('n', 'gd', vim.lsp.buf.definition, { buffer=0 })	-- Va a la definición del simbolo que se encuetra debajo del cursor
-map('n', 'gD', vim.lsp.buf.declaration, { buffer=0 })			-- Salta a la declaracion del objecto
-map('n', '<leader>rn', vim.lsp.buf.rename, { buffer=0 })		-- Salta a los lugares donde se implementa del objecto
 map({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { buffer=0 })	-- Indica las acciones que podemos realizar sobre el simbolo que se encuentra debajo del cursor
 
 map('n', ']d', vim.diagnostic.goto_next, { buffer=0 })	-- Salta al anterior error encotrado
 map('n', '[d', vim.diagnostic.goto_prev, { buffer=0 })	-- Salta al siguiente error encotrado
->>>>>>> 76a8d65 (Keymaps para lsp)
